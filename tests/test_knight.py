43dbb57ed6f592dcf3a4570dbd91274bc0e54e14
Daniel Edades <edadesd@oregonstate.edu>
2018-04-02 11:47:54 -0700

    Adding (failing) test for Knight

--- tests/test_knight.py
+++ tests/test_knight.py
@@ -17,6 +17,11 @@ from piece import PieceColor
 from piece import IllegalPlacementException
 from piece import IllegalMoveException
 
+MAX_RANK = 8
+MIN_RANK = 1
+MIN_FILE = "a"
+MAX_FILE = "h"
+
 
 @pytest.fixture
 def test_board():
@@ -47,6 +52,7 @@ def test_black_knight(test_board):
 class TestCreateKnight:
 
     def test_create_white_knight(self, test_board, test_white_knight):
+        assert test_board
         assert test_white_knight
         assert test_white_knight.color is PieceColor.WHITE
         starting_file = "g"
@@ -63,3 +69,56 @@ class TestCreateKnight:
         starting_space = test_board.get_space(starting_file, starting_rank)
         test_black_knight.place(starting_space)
         assert test_black_knight.current_space is starting_space
+
+
+class TestMoveKnight:
+
+    # Test that the Knight can move two ranks up and one file to the left on a board with no other pieces.
+    def test_move_knight_up_left(self, test_board, test_white_knight):
+        assert test_board
+        assert test_white_knight
+        assert test_white_knight.current_space
+        assert test_white_knight.current_space.rank + 2 <= MAX_RANK
+        assert ord(test_white_knight.current_space.file) > ord(MIN_FILE)
+
+        current_space = test_white_knight.current_space
+        target_file = chr(ord(current_space.file) - 1)
+        target_rank = current_space.rank + 2
+        target_space = test_board.get_space(target_file, target_rank)
+
+        test_white_knight.move(target_space)
+        assert test_white_knight.current_space is target_space
+
+    def test_move_knight_up_right(self, test_board, test_white_knight):
+        assert test_board
+        assert test_white_knight
+        assert test_white_knight.current_space
+        assert test_white_knight.current_space.rank + 2 <= MAX_RANK
+        assert ord(test_white_knight.current_space.file) < ord(MAX_FILE)
+
+        current_space = test_white_knight.current_space
+        target_file = chr(ord(current_space.file) + 1)
+        target_rank = current_space.rank + 2
+        target_space = test_board.get_space(target_file, target_rank)
+
+        test_white_knight.move(target_space)
+        assert test_white_knight.current_space is target_space
+
+    def bad_knight_move(self, test_board, test_white_knight):
+        assert test_board
+        assert test_white_knight
+        assert test_white_knight.current_space
+
+        # Choose a target Space that is not a legal Knight move away from the test Knight's current Space.
+        current_space = test_white_knight.current_space
+        target_rank = test_white_knight.current_space.rank + 1
+        target_space = test_board.get_space(test_white_knight.current_space.file, target_rank)
+
+        # Attempt to move the Knight to the target space, expecting this to cause an IllegalMoveException
+        test_white_knight.move(target_space)
+
+
+
+    def test_bad_knight_move(self, test_board, test_white_knight):
+        with pytest.raises(IllegalMoveException) as info:
+            self.bad_knight_move(test_board, test_white_knight)
