01bab2b4da13340d4bd8a85e9cf0bc7c1a5a5d30
Daniel Edades <edadesd@oregonstate.edu>
2018-04-05 09:03:26 -0700

    Additional failing tests for Rook

--- tests/test_rook.py
+++ tests/test_rook.py
@@ -3,7 +3,7 @@ Tests for the Rook class to ensure correct
 placement, movement, and capturing,
 as well as correct attributes upon creation.
 
-Last modified: 4/4/2018
+Last modified: 4/5/2018
 Author: Daniel Edades
 """
 
@@ -32,16 +32,24 @@ def test_white_rook(test_board):
 
     return test_white_rook
 
-'''
 @pytest.fixture
 def test_white_rook(test_board):
     starting_file = "a"
     starting_rank = 1
     starting_space = test_board.get_space(starting_file, starting_rank)
-    test_knight = Rook(PieceColor.WHITE)
-    test_knight.place(starting_space)
+    test_rook = Rook(PieceColor.WHITE)
+    test_rook.place(starting_space)
     return test_rook
-'''
+
+@pytest.fixture
+def test_black_rook(test_board):
+    starting_file ="a"
+    starting_rank = 8
+    starting_space = test_board.get_space(starting_file, starting_rank)
+    test_rook = Rook(PieceColor.BLACK)
+    test_rook.place(starting_space)
+    return test_rook
+
 
 class TestCreateRook:
 
@@ -72,3 +80,30 @@ class TestMoveRook:
         target_space = test_board.get_space(MAX_FILE, test_white_rook.current_space.rank)
         test_white_rook.move(test_board, target_space)
         assert test_white_rook.current_space is target_space
+
+    def test_move_rook_vertical(self, test_board, test_black_rook):
+        assert test_board
+        assert test_black_rook
+        assert test_black_rook.current_space
+        assert test_black_rook.current_space.rank > MIN_RANK
+
+        target_space = test_board.get_space(test_black_rook.current_space.file, MIN_RANK)
+        test_black_rook.move(test_board, target_space)
+        assert test_black_rook.current_space is target_space
+
+    def bad_rook_move_horizontal(self, test_board, test_white_rook):
+        assert test_board
+        assert test_white_rook
+        assert test_white_rook.current_space
+        assert ord(test_white_rook.current_space.file) + 1 < ord(MAX_FILE)
+
+        target_space = test_board.get_space(MAX_FILE, test_white_rook.current_space.rank)
+        obstacle_space = test_board.get_space(chr(ord(MAX_FILE) - 1), test_white_rook.current_space.rank)
+        obstacle_piece = Rook(PieceColor.BLACK)
+        obstacle_piece.place(obstacle_space)
+
+        test_white_rook.move(test_board, target_space)
+
+    def test_bad_rook_move_horizontal(self, test_board, test_white_rook):
+        with pytest.raises(IllegalMoveException) as info:
+            self.bad_rook_move_horizontal(test_board, test_white_rook)
